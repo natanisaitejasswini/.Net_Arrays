c0e8a84a3bc9839a89acb0bb4c8258e16f6ad6e8
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-03 16:01:30 -0700

    sec

--- Program.cs
+++ Program.cs
@@ -39,41 +39,41 @@ namespace ConsoleApplication
                 }
                 };
             //Directly accessing a multidimensional array
-            Console.WriteLine(array2D[0,1]); //prints 0
-            Console.WriteLine(array3D[1,0,3]); //prints 42
+            Console.WriteLine(array2D[0,1]); //prints 0 because as no value is declared for that.
+            Console.WriteLine(array3D[1,0,3]); //prints 42.. goes to 1 index of main array and then zero index of subarray in that 3rd index
             Console.WriteLine("*******");
             
-            // array except without the need for uniform inner array sizes.
-            // Jagged array declaration
-            int[][] jaggedArray = new int[3][];
-            jaggedArray[0] = new int[5];
-            jaggedArray[1] = new int[4]; 
-            jaggedArray[2] = new int[2];
-            int[][] jaggedArray2 = new int[][] {
-                new int[] {1,3,5,7,9},
-                new int[] {0,2},
-                new int[] {11,22,33,44}
-            };
-            // Short-hand form
-            int[][] jaggedArray3 = {
-                new int[] {1,3,5,7,9},
-                new int[] {0,2},
-                new int[] {11,22,33,44}
-            };
-            // You can even mix jagged and multi-dimensional arrays
-            int[][,] jaggedArray4 = new int[3][,] 
-            {
-                new int[,] { {1,3}, {5,7} },
-                new int[,] { {0,2}, {4,6}, {8,10} },
-                new int[,] { {11,22}, {99,88}, {0,9} } 
-            };
-            // Working through each array in a jagged array
-            foreach(int[] innerArr in jaggedArray){
-                Console.WriteLine("Inner array length is {0}", innerArr.Length);
-            }
-            // Accessing a jagged array
-            Console.WriteLine(jaggedArray3[2][3]); //prints 44
-            Console.WriteLine(jaggedArray2[0][1]); //prints 3
+            // // array except without the need for uniform inner array sizes.
+            // // Jagged array declaration
+            // int[][] jaggedArray = new int[3][];
+            // jaggedArray[0] = new int[5];
+            // jaggedArray[1] = new int[4]; 
+            // jaggedArray[2] = new int[2];
+            // int[][] jaggedArray2 = new int[][] {
+            //     new int[] {1,3,5,7,9},
+            //     new int[] {0,2},
+            //     new int[] {11,22,33,44}
+            // };
+            // // Short-hand form
+            // int[][] jaggedArray3 = {
+            //     new int[] {1,3,5,7,9},
+            //     new int[] {0,2},
+            //     new int[] {11,22,33,44}
+            // };
+            // // You can even mix jagged and multi-dimensional arrays
+            // int[][,] jaggedArray4 = new int[3][,] 
+            // {
+            //     new int[,] { {1,3}, {5,7} },
+            //     new int[,] { {0,2}, {4,6}, {8,10} },
+            //     new int[,] { {11,22}, {99,88}, {0,9} } 
+            // };
+            // // Working through each array in a jagged array
+            // foreach(int[] innerArr in jaggedArray){
+            //     Console.WriteLine("Inner array length is {0}", innerArr.Length);
+            // }
+            // // Accessing a jagged array
+            // Console.WriteLine(jaggedArray3[2][3]); //prints 44
+            // Console.WriteLine(jaggedArray2[0][1]); //prints 3
 
         }
     }
